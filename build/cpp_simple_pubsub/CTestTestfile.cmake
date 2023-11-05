eeacd22d9322702d85fa359057edb563cff134ff
Joshua Peller <jopel@jopel.de>
2023-11-05 13:35:00 +0100

    removed install, build and log from version management

--- build/cpp_simple_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jopel/Documents/01_Coding/ros2-myWorkspace/src/cpp_simple_pubsub
-# Build directory: /home/jopel/Documents/01_Coding/ros2-myWorkspace/build/cpp_simple_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/my_py_package/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jopel/Documents/01_Coding/ros2-myWorkspace/install/my_py_package'
